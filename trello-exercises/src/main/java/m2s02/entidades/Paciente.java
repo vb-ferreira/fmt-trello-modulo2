4b7c66fb6b82de69f74f4eafc0c1512e8554677f
Vinícius <vinicius.ferreira@ufpe.br>
2024-05-20 09:52:48 -0300

    [M2S02] Ex. 01 - Criar classe Paciente

---
+++ trello-exercises/src/main/java/m2s02/entidades/Paciente.java
@@ -0,0 +1,81 @@
+package m2s02.entidades;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ex1: Criar classe Paciente
+ */
+public class Paciente {
+
+    private String nome;
+    private int idade;
+    private double peso;
+    private double altura;
+    private double pressaoArterial;
+    private double frequenciaCardiaca;
+    private String dietaAlimentar;
+    private List<String> atividadesFisicas;
+
+    // Construtor
+    public Paciente() {
+        atividadesFisicas = new ArrayList<>();
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPressaoArterial() {
+        return pressaoArterial;
+    }
+
+    public void setPressaoArterial(double pressaoArterial) {
+        this.pressaoArterial = pressaoArterial;
+    }
+
+    public double getFrequenciaCardiaca() {
+        return frequenciaCardiaca;
+    }
+
+    public void setFrequenciaCardiaca(double frequenciaCardiaca) {
+        this.frequenciaCardiaca = frequenciaCardiaca;
+    }
+
+    public String getDietaAlimentar() {
+        return dietaAlimentar;
+    }
+
+    public void setDietaAlimentar(String dietaAlimentar) {
+        this.dietaAlimentar = dietaAlimentar;
+    }
+}
